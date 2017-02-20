822ac25ee235b44a21fcee4f5c18d66a2ac87c92
Pyc6eH4uk <ruslan.klimov1@yandex.ru>
2017-02-20 12:17:27 +0300

    Try to add score table

--- core/src/com/mygdx/game/ScoreScreen.java
+++ core/src/com/mygdx/game/ScoreScreen.java
@@ -5,38 +5,40 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.Event;
-import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 /**
- * Created by РусБЕНЧик on 19.02.2017.
+ * Created by РусБЕНЧик on 20.02.2017.
  */
 
-public class PlayScreen implements Screen {
+public class ScoreScreen implements Screen {
+    private Attempt1 game;
     private Texture myTexture;
     private TextureRegion myTextureRegion;
     private TextureRegionDrawable myTexRegionDrawable;
     private ImageButton button, scoreBtn;
     private Stage stage;
-    private Attempt1 game;
-    public PlayScreen(Attempt1 aGame) {
+    public ScoreScreen(Attempt1 aGame) {
         game = aGame;
         stage = new Stage(new ScreenViewport());
-        myTexture = new Texture("playBtn.png");
+        myTexture = new Texture("i.jpg");
         myTextureRegion = new TextureRegion(myTexture);
         myTexRegionDrawable = new TextureRegionDrawable(myTextureRegion);
         button = new ImageButton(myTexRegionDrawable);
-        button.setPosition(stage.getWidth()/2 - button.getWidth()/2, stage.getHeight()/2 - button.getHeight()/2);
+        System.out.println(stage.getWidth());
+        button.setPosition(stage.getWidth() - button.getWidth(), stage.getHeight() - button.getHeight());
+
         button.addListener(new InputListener(){
             @Override
             public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                game.setScreen(new GameScreen(game));
+                game.setScreen(new PlayScreen(game));
             }
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
@@ -44,23 +46,7 @@ public class PlayScreen implements Screen {
             }
         });
         stage.addActor(button);
-
-        Texture myTex = new Texture("scoresBtn.png");
-        TextureRegion myTexReg = new TextureRegion(myTex);
-        TextureRegionDrawable myTexRegDraw = new TextureRegionDrawable(myTexReg);
-        scoreBtn = new ImageButton(myTexRegDraw);
-        scoreBtn.setPosition(stage.getWidth()/2 - scoreBtn.getWidth()/2, stage.getHeight()/2 - button.getHeight()/2 - scoreBtn.getHeight());
-        scoreBtn.addListener(new InputListener(){
-            @Override
-            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
-                game.setScreen(new ScoreScreen(game));
-            }
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                return true;
-            }
-        });
-        stage.addActor(scoreBtn);
+        Table table = new Table();
     }
 
     @Override
@@ -71,7 +57,7 @@ public class PlayScreen implements Screen {
     @Override
     public void render(float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0.3f, 1, 0.5f, 1);
         stage.act();
         stage.draw();
     }
